ac39201503b130cdb910acd03f578512faebc369
Ferhat Kuran <60332378+fkuran@users.noreply.github.com>
2021-10-17 22:29:26 +0300

    First Commit

---
+++ maven-demo/src/main/java/com/beat/application.java
@@ -0,0 +1,8 @@
+package com.beat;
+
+public class application {
+	public String Sample() {
+		return "xxx";
+	}
+
+}
---
+++ maven-demo/src/test/java/com/beat/appTest.java
@@ -0,0 +1,14 @@
+package com.beat;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class appTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
